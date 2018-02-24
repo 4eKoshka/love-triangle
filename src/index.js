fe644609a9fbac07c0f72227f6869d565d62d392
Victoria <36647444+4eKoshka@users.noreply.github.com>
2018-02-23 22:08:03 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var k = 0;
+  var f = true;
+  for (var i = 0; i< preferences.length; i++) {
+	   if (preferences.length < 3) {
+		   f=false;
+	   }
+		
+	  	  	  
+	  if ( (i+1) === preferences[preferences[preferences[i]-1]-1] && f === true) {
+		  k++; 
+		 }
+	 	 
+		  
+  }
+  var res = Math.floor(k/3);
+  return res;
 };
